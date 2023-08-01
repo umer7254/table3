dd73227d0e711408813bc2772d39cfc573b01c53
umer7254 <136007876+umer7254@users.noreply.github.com>
2023-08-01 10:05:16 +0500

    table3

---
+++ table(2).js
@@ -0,0 +1,14 @@
+
+
+function table(){
+    document.getElementById('output').innerHTML=' '
+    let y=document.getElementById('nuntable').value
+      for (let x=1; x<11;x++)
+     
+document.getElementById('output').innerHTML+=
+// "$x" +x+" = "+x*2+"<br/>";
+
+`${y}x${x}=${x*y} <br>`
+
+
+}
